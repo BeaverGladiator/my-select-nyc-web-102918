eb61d3e440301f2a605cf0208eda23d46f8a5411
Matthew Migliore <mmigliore311@gmail.com>
2018-10-16 15:38:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
     i = 0 
     selected_collection =  []
     while i < collection.length 
-      selected_collection << yield(collection[i])
+      yield(selected_collection << collection[i])
       i = i + 1 
     end 
   
